a8fbc3630aba4fd48310b1236bdb73e83034b2bb
icelandia <ice-return@hotmail.com>
2023-02-09 11:57:08 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        sum(1,4);
+
+
+    }
+    public static void sum(int i, int j)
+    {
+        System.out.print(i+j);
+    }
+}
